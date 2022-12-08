63d32403918786b6595d468c12512f0a0ba5dddc
Hamza Hafeez <hamza.hafeez@digifloat.com>
2022-12-08 11:39:09 +0500

    Requirement file added

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #Execution Time - Minutes 
-stream_runtime=2
+stream_runtime=60
 
 search_terms = ['formula 1', 'f1', 'formula1']
 
 #Directory to store files
 file_prefix = 'ingestions/'
-error_prefix = 'errors/'
+logs_prefix = 'logs/'
--- main.py
+++ main.py
@@ -4,15 +4,23 @@ from tweepy import StreamRule
 from twitter_client import Streaming
 # from def_secrets import consumer_key, consumer_secret, access_key, access_secret
 from dotenv import load_dotenv
-
 load_dotenv()
 
 bearer_token  = str(os.environ["BEARER_TOKEN"])
+# consumer_key = os.environ["API_KEY"]
+# consumer_secret = os.environ["API_KEY_SECRET"]
+# access_token = os.environ["ACCESS_TOKEN"]
+# access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
 
 def main():
+    # auth = OAuth1UserHandler(
+    #     consumer_key, consumer_secret, access_token, access_token_secret
+    # )
 
+    # api = API(auth)
     streamer = Streaming(bearer_token)    
+    # streamer.add_rules(StreamRule("f1 OR (formula 1) OR formula1 lang:en"))
     streamer.add_rules(StreamRule("f1 OR formula1 lang:en"))
 
     try:
